ba54285ca364f96f71a014a0f226aa813b35e766
can-dy-jack <jjack_chen3@163.com>
2020-12-04 10:45:59 +0800

    v2.0.0

---
+++ app/src/main/java/com/ckh/jim/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.ckh.jim
+
+import android.app.Application
+import android.content.Context
+import com.umeng.commonsdk.UMConfigure
+
+class MyApplication: Application() {
+
+    lateinit var context: Context
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        UMConfigure.init(context, "5fc99c45094d637f31345988", "", UMConfigure.DEVICE_TYPE_PHONE, "")
+    }
+}
